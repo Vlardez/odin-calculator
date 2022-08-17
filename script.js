5afad64aa33e61a96e86780021d4316f56176c54
Vlad Popa <caim_magnus@hotmail.com>
2022-08-17 15:27:32 -0400

    tried adding funcs to clear and add to numbers to display

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+//initializing some variables and attaching buttons to variables
+let firstNum = 0;
+let nextNum = 0;
+let clearBtn = document.querySelector('#clearbtn');
+let display = document.querySelector('#display');
+let numBtns = document.querySelectorAll('.numbtn');
+let dispVal = 0;
+//adding onclicks to various buttons
+clearBtn.addEventListener('click', e => {
+    display.textContent = "";
+    dispVal = 0;
+})
+numBtns.forEach(numBtn => {
+    numBtn.addEventListener('click', e => {
+        disp(this.value);
+    })
+})
+
+//operator functions, display functions, etc
 function add(a,b) {
     return a+b;
 }
@@ -21,4 +40,8 @@ function operate(op,a,b) {
         case '/':
             return divide(a,b);
     }
+}
+function disp(num) {
+    dispVal += num;
+    display.textContent = dispVal;
 }
