06e1f81c870da439f8fdf3ff7f61a192a5614386
Vlad Popa <caim_magnus@hotmail.com>
2022-08-18 10:10:35 -0400

    Tried to add the operations and equals functions.

--- script.js
+++ script.js
@@ -1,20 +1,60 @@
 //initializing some variables and attaching buttons to variables
-let firstNum = 0;
-let nextNum = 0;
+let firstNum;
+let nextNum;
 let clearBtn = document.querySelector('#clearbtn');
 let display = document.querySelector('#display');
 let numBtns = document.querySelectorAll('.numbtn');
-let dispVal = 0;
+let dispVal;
+let deciBtn = document.querySelector('#decibtn');
+let opBtns = document.querySelectorAll('.opbtn');
+let eqBtn = document.querySelector('#equalbtn');
+const opQueue = {
+    queue: false,
+    op: ""
+};
+
 //adding onclicks to various buttons
 clearBtn.addEventListener('click', e => {
     display.textContent = "";
-    dispVal = 0;
+    dispVal = null;
 })
 numBtns.forEach(numBtn => {
     numBtn.addEventListener('click', e => {
-        disp(this.value);
+        disp(e.currentTarget.value);
     })
 })
+deciBtn.addEventListener('click',e => {
+    if (display.textContent !== "" && display.textContent.search(/./) === -1) {
+        disp(e.currentTarget.value);
+    }
+})
+opBtns.forEach(opBtn => {
+    opBtn.addEventListener('click', e => {
+            if (!opQueue.queue) {
+                firstNum = dispVal;
+                dispVal = null;
+                display.textContent = "";
+                opQueue.queue = true;
+                opQueue.op = e.currentTarget.value;
+            } else {
+                nextNum = dispVal;
+                dispVal = operate(opQueue.op,firstNum,nextNum);
+                display.textContent = dispVal;
+                firstNum = dispVal;
+            }
+    })
+})
+eqBtn.addEventListener('click', e => {
+    if (opQueue.queue) {
+        nextNum = dispVal;
+        dispVal = operate(opQueue.op,firstNum,nextNum);
+        display.textContent = dispVal;
+        firstNum = dispVal
+        opQueue.queue = false;
+        opQueue.op = "";
+    }
+})
+
 
 //operator functions, display functions, etc
 function add(a,b) {
@@ -43,5 +83,5 @@ function operate(op,a,b) {
 }
 function disp(num) {
     display.textContent += num;
-    dispVal = parseInt(display.textContent);
+    dispVal = parseFloat(display.textContent);
 }
