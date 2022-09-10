602f5297f974e97878bb4911c3e967c0cb0dfeb1
Vlad <m.vlad.popa@gmail.com>
2022-09-10 11:43:26 -0400

    completed keyboard functionality for all operations

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ const opStates = {
 
 //attempting to add keyboard functions
 document.addEventListener('keydown', e => {
-    console.log(e.key);
     switch (e.key) {
         case 'Delete':
             clear();
@@ -29,80 +28,124 @@ document.addEventListener('keydown', e => {
         case '=':
             equals();
             break;
+        case 'Enter':
+            e.preventDefault();
+            equals();
+            break;
         case '+':
-            
+            performOp(e);
+            break;
+        case '*':
+            performOp(e);
+            break;
+        case '/':
+            performOp(e);
+            break;
+        case '-':
+            performOp(e);
+            break;
+        case '%':
+            percVal();
+            break;
+        case 'Backspace':
+            backspace();
+            break;
+        case '.':
+            dispDeci(e);
+            break;
+        case '0':
+            dispZero(e);
+            break;
     }
-    if (e.key >= 0 && e.key <= 9) {
-        if (mainDisplay.textContent === "0" || opStates.clrdisp) {
-            mainDisplay.textContent = "";
-            opStates.clrdisp = false;
-        }
-        disp(e.key);
+    if (e.key >= 1 && e.key <= 9) {
+        dispNum(e);
     }
 })
 
 //adding onclicks to various buttons
-clearBtn.addEventListener('click', clear)
+clearBtn.addEventListener('click', clear);
 
 negBtn.addEventListener('click', e => {
     dispVal = -dispVal;
     mainDisplay.textContent = dispVal;
-})
-percBtn.addEventListener('click', e => {
-    dispVal = dispVal/100;
-    mainDisplay.textContent = dispVal;
-})
+});
+
+percBtn.addEventListener('click', percVal);
+
 numBtns.forEach(numBtn => {
     numBtn.addEventListener('click', dispNum)
-})
-deciBtn.addEventListener('click', e => {
-    if (mainDisplay.textContent !== "" && mainDisplay.textContent.search(/\./) === -1) {
-        disp(e.currentTarget.value);
-    }
-})
-zeroBtn.addEventListener('click', e => {
-    if (mainDisplay.textContent === "" || mainDisplay.textContent.search(/[1-9]/) !== -1) {
-        disp(e.currentTarget.value);
-    }
-})
+});
+
+deciBtn.addEventListener('click', dispDeci);
+
+zeroBtn.addEventListener('click', dispZero);
+
 opBtns.forEach(opBtn => {
-    opBtn.addEventListener('click', e => {
-        if (opStates.clrsub) {
-            opStates.clrsub = false;
-            subDisplay.textContent = "";
-        }
-        if (!opStates.queue) {
-            subDisplay.textContent += (dispVal + " " + e.currentTarget.value);
-            firstNum = dispVal;
-            mainDisplay.textContent = "";
-            opStates.queue = true;
-            opStates.op = e.currentTarget.value;
-        } else {
-            subDisplay.textContent += (" " + dispVal + " " + e.currentTarget.value);
-            calc();
-            opStates.clrdisp = true;
-            opStates.op = e.currentTarget.value;
-        }
-    })
+    opBtn.addEventListener('click', performOp)
 })
 eqBtn.addEventListener('click', equals)
 
+
 //functions refactored here to implement keyboard functionality
+function backspace() {
+    mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
+    dispVal = parseFloat(mainDisplay.textContent);
+}
+
 function dispNum(e) {
+    let val = e.type === 'click' ? e.currentTarget.value : e.key;
     if (mainDisplay.textContent === "0" || opStates.clrdisp) {
         mainDisplay.textContent = "";
         opStates.clrdisp = false;
     }
-    disp(e.currentTarget.value);
+    disp(val);
+}
+
+function dispZero(e) {
+    let val = e.type === 'click' ? e.currentTarget.value : e.key;
+    if (mainDisplay.textContent === "" || mainDisplay.textContent.search(/[1-9]/) !== -1) {
+        disp(val);
+    }
+}
+
+function dispDeci(e) {
+    let val = e.type === 'click' ? e.currentTarget.value : e.key;
+    if (mainDisplay.textContent !== "" && mainDisplay.textContent.search(/\./) === -1) {
+        disp(val);
+    }
+}
+
+function percVal() {
+    dispVal = dispVal / 100;
+    mainDisplay.textContent = dispVal;
 }
 
 function equals() {
     if (opStates.queue) {
-        console.log('equals ran')
         subDisplay.textContent += (" " + dispVal);
         calc();
         clrQueue();
         opStates.clrsub = true;
+    }
+}
+
+function performOp(e) {
+    let val = e.type === 'click' ? e.currentTarget.value : e.key;
+    if (opStates.clrsub) {
+        opStates.clrsub = false;
+        subDisplay.textContent = "";
+    }
+    if (!opStates.queue) {
+        subDisplay.textContent += (dispVal + " " + val);
+        firstNum = dispVal;
+        mainDisplay.textContent = "";
+        opStates.queue = true;
+        opStates.op = val;
+    } else {
+        subDisplay.textContent += (" " + dispVal + " " + val);
+        calc();
+        opStates.clrdisp = true;
+        opStates.op = val;
     }
 }
 
@@ -144,10 +187,12 @@ function disp(num) {
     mainDisplay.textContent += num;
     dispVal = parseFloat(mainDisplay.textContent);
 }
+
 function clrQueue() {
     opStates.queue = false;
     opStates.op = "";
 }
+
 function calc() {
     nextNum = dispVal;
     dispVal = operate(opStates.op,firstNum,nextNum);
